c7028825c4dc4589cea330ab7c74a1e37eda6f07
Coro-sensei <acoro1719@gmail.com>
2025-04-10 20:35:46 +0800

    modified:   asking for inputs on the questions and the 4 possible answers

--- quiz_creator.py
+++ quiz_creator.py
@@ -4,6 +4,10 @@
 # Ask another question until the user chose to exit
 
 # Ask the user for a question
+question_input = input("Enter your question: ")
+
 # Ask the user to input 4 possible with a correct answer in those 4 possible answer
+question_answer = input("Enter 4 possible answers: ")
+
 # Put the inputs in a text file 
 # Ask until chose to exit
