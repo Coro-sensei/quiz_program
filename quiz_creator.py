032567ca4c8fa5f7ff0a1144adf13074e1eb92b9
Coro-sensei <acoro1719@gmail.com>
2025-04-10 20:26:10 +0800

    modified:  expanded the pseudocode

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,9 @@
 # Create a program that ask user for a question,
 # it will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
 # Write the collected data to a text file. 
-# Ask another question until the user chose to exit.
+# Ask another question until the user chose to exit
+
+# Ask the user for a question
+# Ask the user to input 4 possible with a correct answer in those 4 possible answer
+# Put the inputs in a text file 
+# Ask until chose to exit
