9b866c80f219f3e762f422cda28529e4da1dff9f
Coro-sensei <acoro1719@gmail.com>
2025-04-12 22:55:11 +0800

    new file:   pseudocode for the quiz maker

---
+++ quiz_program/quiz_maker.py
@@ -0,0 +1,10 @@
+# Create a program that ask user for a question,
+# It will also ask for 4 possible answer (a,b,c,d) and the correct answer. 
+# Write the collected data to a text file. Ask another question until the user chose to exit.
+
+# Make the GUI 
+# Do the code for the quiz maker itself 
+# Input questions
+# Input 4 possible answers 
+# Input the correct answer 
+# Write the data to a text file
