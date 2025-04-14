bbf649d5501d22cf4257110df4ce7c219ee550ea
Coro-sensei <acoro1719@gmail.com>
2025-04-15 00:15:26 +0800

    adjusted the title image proportions. and made the background yellow

--- quiz_program/quiz_maker.py
+++ quiz_program/quiz_maker.py
@@ -58,22 +58,20 @@ central_frame.place(relx = 0.5, rely = 0.5, anchor = "center")
 # Title label image 
 try:
     title_label = Image.open("pokeball_title.png")
-    title_label = title_label.resize((600, 150), Image.LANCZOS) # Image filter and resize 
+    title_label = title_label.resize((650, 250), Image.LANCZOS) # Image filter and resize 
     title_image = ImageTk.PhotoImage(title_label)
 
     # Position of the title label
-    title_pos = tk.Label(window_root, image = title_image)
+    title_pos = tk.Label(window_root, image = title_image, bg = "yellow")
     title_pos.image = title_image
 
     title_pos_y = (window_height // 2) - 200
-    title_pos.place(relx = 0.5, rely = 0.5, anchor = "center", y = -title_pos_y)
+    title_pos.place(relx = 0.5, rely = 0.43, anchor = "center", y = -title_pos_y)
     
 except Exception as e: # This is if the image suddenly does load
     title_label_2 = tk.Label(window_root, text = "Pokemon Quiz Maker", font = tkFont.Font(family = "Pokemon Hollow"), bg = "yellow")
     title_label_2.place(relx = 0.5, rely = 0.3, anchor = "center")
     messagebox.showerror("Title image error")
-
-
     
 # Center the contents 
 central_frame.grid_columnconfigure(0, weight = 1)
