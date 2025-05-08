9ec661108c59bb5a7d570fb1fd0103f4ac9b2ac6
Coro-sensei <acoro1719@gmail.com>
2025-05-08 22:16:52 +0800

    import the libraries needed

--- quiz_program_taker/quiz_taker.py
+++ quiz_program_taker/quiz_taker.py
@@ -1,3 +1,8 @@
 # Create a  quiz taker program 
 # Create a GUI for the program
 # Use the text file of quiz maker
+
+# Make the GUI
+import tkinter as tk 
+from tkinter import messagebox
+import os 
