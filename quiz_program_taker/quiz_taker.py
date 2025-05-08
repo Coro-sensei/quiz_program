dfb46931a4ae1d22c441799c2471f92bb6104710
Coro-sensei <acoro1719@gmail.com>
2025-05-08 22:43:14 +0800

    if continue checks if the data is existing

--- quiz_program_taker/quiz_taker.py
+++ quiz_program_taker/quiz_taker.py
@@ -7,3 +7,13 @@ import tkinter as tk
 from tkinter import messagebox
 import os 
 from PIL import Image, ImageTk
+
+quiz_data = []
+
+if os.path.exists("quiz_maker_data.txt"):
+    with open("quiz_maker_data.txt", "r") as f:
+        blocks = f.read().strip().split("-" * 40)
+        for blocks in blocks:
+            lines = blocks.strip().split("\n")
+            if len(lines) < 6:
+                continue
